c351a8b0c56c69b617c67582917eceff9169723e
matheus2306 <math2306.r@gmail.com>
2025-02-26 16:05:10 -0300

    Refactor layout and update image import in App component

--- 03-devlinks/src/App.jsx
+++ 03-devlinks/src/App.jsx
@@ -3,7 +3,7 @@ import Links from "./components/links/Links";
 import Profile from "./components/profile/profile";
 import Switch from "./components/switch/Switch";
 import "./App.css";
-import photolight from "./img/baixados.gif"
+import photolight from "./img/baixados (1).gif"
 import photodark from "./img/Google revela os 10 GIFs mais procurados pelos brasileiros em 2018 - Mega Curioso.gif"
 import SocialLinks from "./components/socialLinks/SocialLinks";
 import Footer from "./components/Footer/Footer";
@@ -18,6 +18,7 @@ const App = () => {
   };
   return (
     <div id="App" className={islight ? "light" : ""}>
+      <div id="container">
       <Profile img={islight ? photolight : photodark}>@MatheusFRodrigues</Profile>
       <Switch toggle={toggle} isLight={islight} />
       <div id="links">
@@ -36,7 +37,7 @@ const App = () => {
         <SocialLinks
           link={"https://github.com/Matheus2306"}
           icon={"logo-github"}
-        />
+          />
         <SocialLinks
           link={"https://www.instagram.com/math2306.r/"}
           icon={"logo-instagram"}
@@ -46,12 +47,13 @@ const App = () => {
             "https://www.linkedin.com/in/matheus-felipe-rodrigues-6b4a6a33a/?trk=opento_sprofile_topcard"
           }
           icon={"logo-linkedin"}
-        />
+          />
         <SocialLinks
           link={"https://www.youtube.com/@Math_rZezin"}
           icon={"logo-youtube"}
-        />
+          />
       </div>
+          </div>
       <Footer>MatheusFRodrigues</Footer>
     </div>
   );
