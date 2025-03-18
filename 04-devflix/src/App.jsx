81fb211505b62b522db0111e4b176a200b051305
matheus2306 <math2306.r@gmail.com>
2025-03-18 16:17:34 -0300

    remove moviecard

--- 04-devflix/src/App.jsx
+++ 04-devflix/src/App.jsx
@@ -1,5 +1,4 @@
 import "./App.css";
-import MovieCard from "./components/MovieCard/MovieCard";
 import Footer from "./components/Footer/Footer";
 import { useEffect, useState } from "react";
 import logo from "./assets/devflix.png";
@@ -11,7 +10,7 @@ const App = () => {
 
   //utilizando chave de API do arquivo .env
 
-  const apiKey = "e4d577fa"
+  const apiKey = "e4d577fa";
   const apiUrl = `https://omdbapi.com/?apikey=${apiKey}`;
 
   //alimentando com dados para não ficar nulo
@@ -43,24 +42,11 @@ const App = () => {
             type="text"
             placeholder="Pesquise por filmes"
           />
-          <img
-            onClick={() => searchMovies(search)}
-            src={lupa}
-            alt=""
-          />
+          <img onClick={() => searchMovies(search)} src={lupa} alt="" />
         </div>
       </div>
       {/* mapeando o array de "movies" ou seja buscando os dados guardados na API ou array */}
-      {movies?.length > 0 ? (
-        <div className="container">
-          {movies.map((movie, index) => (
-            <MovieCard key={index} apiUrl={apiUrl} {...movie} />
-            // ...movie traz todos os dados guardados no DB ou array
-          ))}
-        </div>
-      ) : (
-        <h2 className="empty">Filme não encontrado</h2>
-      )}
+
       <Footer
         DEVname="Matheus Felipe Rodrigues"
         DEVLInk="https://github.com/Matheus2306"
