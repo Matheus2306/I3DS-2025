22e69a881091b0798aef052c8f856c300ed31206
matheus2306 <math2306.r@gmail.com>
2025-03-18 14:28:54 -0300

    Refactor MovieCard and Moviedescription components; adjust layout styles and implement modal functionality for movie details display

--- 04-devflix/src/components/Moviecard/Moviecard.jsx
+++ 04-devflix/src/components/Moviecard/Moviecard.jsx
@@ -1,22 +1,30 @@
+import { useState } from "react";
 import Moviedescription from "../Moviedescription/Moviedescription";
 import styles from "./Moviecard.module.css";
 
-const MovieCard = ({apiUrl, ...props}) => {
+const MovieCard = (props) => {
+  const [isMdalOpen, setModalOpen] = useState(false);
+  const toggleModal = () => {
+    setModalOpen(!isMdalOpen)
+  };
+
   return (
     <>
-    <div className={styles.movie}>
-      <div>
-        <p>{props.Year}</p>
+      <div onClick={toggleModal} className={styles.movie}>
+        <div>
+          <p>{props.Year}</p>
+        </div>
+        <div>
+          <img src={props.Poster} alt="" />
+        </div>
+        <div>
+          <span>{props.Type}</span>
+          <h3>{props.Title}</h3>
+        </div>
       </div>
-      <div>
-        <img src={props.Poster} alt="" />
-      </div>
-      <div>
-        <span>{props.Type}</span>
-        <h3>{props.Title}</h3>
-      </div>
-    </div>
-      <Moviedescription apiUrl={apiUrl} movieId={props.imdbID}/>
+      {isMdalOpen && (
+        <Moviedescription click={toggleModal} apiUrl={props.apiUrl} movieId={props.imdbID} />
+      )}
     </>
   );
 };
--- 04-devflix/src/components/Moviedescription/Moviedescription.jsx
+++ 04-devflix/src/components/Moviedescription/Moviedescription.jsx
@@ -1,11 +1,59 @@
-import styles from "./Moviedescription.module.css"
+// Importa os estilos específicos para este componente
+import styles from "./Moviedescription.module.css";
+import devflix from "../../../public/favicon.png";
+// Importa os hooks useEffect e useState do React
+import { useEffect, useState } from "react";
 
-const Moviedescription = ({apiUrl, movieId}) => {
+// Define o componente funcional Moviedescription que recebe props como argumento
+const Moviedescription = (props) => {
+  // Declara um estado chamado movieDesc e uma função setMovieDesc para atualizá-lo, inicializado como um array vazio
+  const [movieDesc, setMovieDesc] = useState([]);
+
+  // useEffect é um hook que executa um efeito colateral. Neste caso, ele será executado após cada renderização do componente
+  useEffect(() => {
+    // Função que fará a conexão com a API e trará os dados do filme baseado no link fornecido
+    fetch(`${props.apiUrl}&i=${props.movieId}`) // Faz uma requisição à API usando a URL e o ID do filme passados via props
+      .then((response) => response.json()) // Converte a resposta da API para JSON
+      .then((data) => setMovieDesc(data)) // Atualiza o estado movieDesc com os dados recebidos da API
+      .catch((error) => console.error(error)); // Captura e exibe qualquer erro que ocorrer durante a requisição
+  }, []);
+
   return (
-<>
+    <div className={styles.modalBackdrop} onClick={props.click}>
+      <div className={styles.movieModal} onClick={(e) => e.stopPropagation()}>
+        <div className={styles.movieInfo}>
+          <img src={movieDesc.Poster} alt={props.Title} />
+          <button className={styles.btnClose} onClick={props.click}>
+            X
+          </button>
+          <div className={styles.movieType}>
+            <div>
+              <img src={devflix} alt="" />
+              {movieDesc.Type}
+              <h1>{movieDesc.Title}</h1>
+              <a href="https://google.com" target="_blank">
+                Assista
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className={styles.containerMisc}>
+          <div className={styles.containerFlex}>
+            Avaliação : {movieDesc.imdbRating} | Duração {movieDesc.Runtime} |{" "}
+            {movieDesc.Released}
+          </div>
+        </div>
+        <div className={styles.desc}>
+          <div className={styles.containerFlex}>
+            <p>Elenco: {movieDesc.Actors}</p>
+            <p>Gênero: {movieDesc.Genre}</p>
+          </div>
+          <p>Sinopse: {movieDesc.Plot}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-</> 
-)
-}
-
-export default Moviedescription
+// Exporta o componente Moviedescription para que possa ser utilizado em outros arquivos
+export default Moviedescription;
