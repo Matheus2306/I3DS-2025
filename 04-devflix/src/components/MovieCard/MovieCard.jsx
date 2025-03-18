c40f294ffb70b59796cf42fba0fbb22ef651eb12
matheus2306 <math2306.r@gmail.com>
2025-03-18 16:25:50 -0300

    ajustes3

---
+++ 04-devflix/src/components/MovieCard/MovieCard.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import Moviedescription from "../Moviedescription/Moviedescription";
+import styles from "./MovieCard.module.css";
+const MovieCard = (props) => {
+  const [isMdalOpen, setModalOpen] = useState(false);
+  const toggleModal = () => {
+    setModalOpen(!isMdalOpen);
+  };
+
+  return (
+    <>
+      <div onClick={toggleModal} className={styles.movie}>
+        <div>
+          <p>{props.Year}</p>
+        </div>
+        <div>
+          <img src={props.Poster} alt="" />
+        </div>
+        <div>
+          <span>{props.Type}</span>
+          <h3>{props.Title}</h3>
+        </div>
+      </div>
+      {isMdalOpen && (
+        <Moviedescription
+          click={toggleModal}
+          apiUrl={props.apiUrl}
+          movieId={props.imdbID}
+        />
+      )}
+    </>
+  );
+};
+
+export default MovieCard;
